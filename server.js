2623648ace44c746e019362074597742e0900a06
mahiro-25 <mahiroc95@gmail.com>
2025-05-06 01:33:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static("public"));
 
 const conversationHistory = {};
